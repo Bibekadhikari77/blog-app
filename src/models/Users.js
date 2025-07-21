c411ca1c7fd887d7698607bc6541c20946a36285
Bibek Adhikari <adhikaribibek84@gmail.com>
2025-07-21 12:58:55 +0545

    change blogRoutes in line number 8 and 9

--- src/models/Users.js
+++ src/models/Users.js
@@ -14,7 +14,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ['user', 'admin'],
         default: 'user'
-     }
+     },
+
+     password: {
+        type: String,
+        required: true
+        }
 })
 
 module.exports = mongoose.model('User', userSchema)
--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -5,10 +5,10 @@ const upload = require('../middlewares/multer');
 
 const router = express.Router()
 
-router.post('/create', protect,authorizeRoles("admin"), upload.array('images', 5) ,createBlog); // Assuming createBlog is defined in blogControllers.js
-router.get('/get-blogs', protect, getAllBlogs); // Assuming getAllBlogs is defined in blogControllers.js
+router.post('/create', protect,authorizeRoles("admin"), upload.array('images', 5) ,createBlog); 
+router.get('/get-blogs', protect, getAllBlogs); 
 router.delete('/deleteblog/:id', protect,authorizeRoles("admin"), deleteBlog);
-router.get('/get-blog/:id', protect, getSingleBlog); // Assuming getSingleBlog is defined in blogControllers.js
+router.get('/get-blog/:id', protect, getSingleBlog); 
 router.put('/update-blog/:id', protect, authorizeRoles("admin"), upload.array('images', 5), updateBlog);
 
 module.exports = router;
